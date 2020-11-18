958c594d555153b467487a8ff84071c04d3738a0
Somme Sakounthong <sommes.email@gmail.com>
2020-11-18 10:41:40 +0000

    updated home

--- src/page-components/frontpage/index.js
+++ src/page-components/frontpage/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
 import Layout from 'components/layout';
-import Grid, { GridItem } from 'components/grid';
 import { simplyFetchFromGraph } from 'lib/graph';
 import fragments from 'lib/graph/fragments';
-//import { useT } from 'lib/i18n';
 import { Header } from 'ui';
 import { Outer } from './styles';
 import ShapeComponents from 'components/shape/components';
-import ItemMicroformat from 'components/item-microformat';
-import { List } from '../folder/styles';
+
 export async function getData({ language, preview = null }) {
   try {
     const { data } = await simplyFetchFromGraph({
@@ -29,7 +26,7 @@ export async function getData({ language, preview = null }) {
         version: preview ? 'draft' : 'published'
       }
     });
-    //console.log(data.catalogue.components)
+
     return { ...data, preview };
   } catch (error) {
     console.log(error);
@@ -38,42 +35,14 @@ export async function getData({ language, preview = null }) {
 }
 
 export default function FrontPage({ catalogue, preview }) {
-  //const t = useT();
-
-  const { children } = catalogue;
-
-  const gridRelations = catalogue.components
-    ?.filter((c) => c.type === 'gridRelations')
-    ?.reduce((acc, g) => [...acc, ...(g?.content?.grids || [])], []);
-  const rest = catalogue.components?.filter((c) => c.type !== 'gridRelations');
-
-  //console.log(gridRelations);
+  const componentsRest = catalogue?.components?.filter((c) => c.name);
 
   return (
     <Layout title={catalogue.name} preview={preview}>
       <Outer>
         <Header centerContent>
-          <ShapeComponents components={rest} />
+          <ShapeComponents components={componentsRest} />
         </Header>
-      </Outer>
-      <Outer>
-        {gridRelations?.length > 0
-          ? gridRelations?.map((grid, index) => (
-              <Grid
-                key={index}
-                model={grid}
-                cellComponent={({ cell }) => (
-                  <GridItem data={cell.item} gridCell={cell} />
-                )}
-              />
-            ))
-          : children && (
-              <List>
-                {children.map((item, i) => (
-                  <ItemMicroformat item={item} key={i} />
-                ))}
-              </List>
-            )}
       </Outer>
     </Layout>
   );
