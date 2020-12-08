273cf5080029cf695bd783251e9e1bfbbece5104
Somme Sakounthong <sommes.email@gmail.com>
2020-12-08 12:14:43 +0000

    added new ga tag

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -64,11 +64,15 @@ export default function Layout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                   })(window,document,'script','dataLayer','GTM-KT6RKCV');`
+            __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GATAG}"></script>
+                    <script>
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${process.env.NEXT_PUBLIC_GATAG}', { 'anonymize_ip': true });
+                    </script>
+                    {% endif %}`
           }}
         />
         <title key="title">{`${shopName} - ${title}`}</title>
