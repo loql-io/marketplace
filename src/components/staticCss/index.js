3eecfafae17a3254b6bc30754dd0d8da117a692c
Somme Sakounthong <sommes.email@gmail.com>
2021-03-19 11:46:05 +0000

    css

---
+++ src/components/staticCss/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Head from 'next/head';
+
+export const StaticCss = () => (
+  <Head>
+    <link rel="stylesheet" type="text/css" href="/static/css/home.css" />
+  </Head>
+);
+
+export default StaticCss;
